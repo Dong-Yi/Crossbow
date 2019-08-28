a3e552b34abc384a08b4c34e54dcf829ef163dc2
dong-yi <2922102962@qq.com>
2019-08-28 20:13:42 +0800

    :)

--- src/Crossbow/Main.php
+++ src/Crossbow/Main.php
@@ -17,7 +17,7 @@ class Main extends PluginBase implements Listener {
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		ItemFactory::registerItem(new Crossbow());
-		Item::addCreativeItem(new Crossbow());
+		Item::initCreativeItems();
 	}
 	
 	public function onDataPacketReceive(DataPacketReceiveEvent $ev) {
